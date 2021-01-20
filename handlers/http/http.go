b6cec0474ec23fa1cd3b21738c8316ceafe50c5d
Luca <lucat1@users.noreply.github.com>
2021-01-20 12:49:35 +0100

    wip

--- handlers/http/http.go
+++ handlers/http/http.go
@@ -6,8 +6,11 @@ import (
 	"github.com/lucat1/magik"
 )
 
+// Handles requests to the */reigster.. path authenticating
+// the user who provides a valid token and redirecting the request appropriately
+// TODO: set the cookie on the client and prob generate a different one with a different expiry date
 func RegisterFunc(m *magik.Magik) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		token := r.URL.Query().Get("t")
 		backto := r.URL.Query().Get("r")
 		// email
@@ -19,9 +22,20 @@ func RegisterFunc(m *magik.Magik) http.HandlerFunc {
 
 		// TODO: accept the email internally
 		http.Redirect(w, r, backto, http.StatusMovedPermanently)
-	})
+	}
 }
 
 func Register(m *magik.Magik) http.Handler {
 	return http.Handler(RegisterFunc(m))
+}
+
+// checks if the user is authenticated and sets the appropriate values in the context
+// based on that. then it calls the h handler to properly manage the request
+func WithAuthFunc(m *magik.Magik, h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
+
+func WithAuth(m *magik.Magik, h http.Handler) http.Handler {
+	return http.Handler(WithAuthFunc(m, h))
 }
